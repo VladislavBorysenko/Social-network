1a6ffdf32fdef6c1b54cf53c14098eef20af282a
VladislavBorysenko <legionyipa@gmail.com>
2021-09-16 19:44:22 +0300

    Perenesli dannye v index.sj

---
+++ src/redux/state.js
@@ -0,0 +1,29 @@
+let state = {
+
+    profilePage: {
+        posts: [
+            {id: 1, message: 'Hi, how are you', likeCount: 13},
+            {id: 2, message: 'This my first post', likeCount: 15},
+            {id: 3, message: 'Hello word', likeCount: 10},
+            {id: 4, message: 'Yo, whats up', likeCount: 3}
+        ]
+    },
+    dialogsPage: {
+        dialogs: [
+            {id: 1, name: 'Dimych'},
+            {id: 2, name: 'Pavel'},
+            {id: 3, name: 'Vlad'},
+            {id: 4, name: 'Andrey'},
+            {id: 5, name: 'Maya'},
+            {id: 6, name: 'Masha'}
+        ],
+        messages: [
+            {id: 1, message: 'Hello'},
+            {id: 2, message: 'Hi'},
+            {id: 3, message: 'Whats up'},
+            {id: 4, message: 'Yo'}
+        ]
+    }
+}
+
+export default state;
